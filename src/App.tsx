e64f37b1dc6fdca753d284d3b61e3c0cf1182253
Amanda Roche <142677539+stitchpetri@users.noreply.github.com>
2025-09-19 10:16:39 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-					<div> <p> Hi this is working </p>
-					</div>
+    <> <div> <p> Hi this is working </p> </div>
     </>
   )
 }
